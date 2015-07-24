7d294427c57b40a23c9fb94af3552d20a30aa0c8
Pardeep Saini <pardeep.saini@netsolutionsindis.com>
2015-07-24 12:49:52 +0530

    fix model issues

--- app/models/secretariat.rb
+++ app/models/secretariat.rb
@@ -1,4 +1,4 @@
-# class Secretariat < ActiveRecord::Base
+class Secretariat < ActiveRecord::Base
 	attr_accessor :image #If exists
 	mount_uploader :image, ImageUploader
 	validates :position, :presence => true,:format => { :with => /\A[a-zA-Z\s\-]+\z/},length: { maximum: 100}
@@ -7,11 +7,12 @@
 	validates :major,:presence => true,:format => { :with => /\A[a-zA-Z\s\.',:(){}\-]+\z/},length: { maximum: 100}
 	validates :description,:presence => true,:format => { :with => /\A[a-zA-Z0-9\s\.',:(){}_\-]+\z/},length: { maximum: 1000}
 	validates :image,:presence => true
-    validate :validate_minimum_image_size
-    def validate_minimum_image_size
-     image1 = MiniMagick::Image.open(image.path)
-      unless image1[:width] < 500 && image1[:height] < 500
-        errors.add :image, "should be 500x500px maximum!" 
-      end
+  validate :validate_minimum_image_size
+  
+  def validate_minimum_image_size
+    image1 = MiniMagick::Image.open(image.path)
+    unless image1[:width] < 500 && image1[:height] < 500
+      errors.add :image, "should be 500x500px maximum!" 
     end
+  end
 end
