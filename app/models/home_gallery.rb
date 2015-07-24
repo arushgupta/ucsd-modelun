9162976da55d20799ea2f9a6968a7fad783b81fa
anshu <anshu.sharma@netsolutions.in>
2015-07-24 12:35:56 +0530

    apply_now and Home_gallery model

---
+++ app/models/home_gallery.rb
@@ -0,0 +1,15 @@
+class HomeGallery < ActiveRecord::Base
+	attr_accessor :image_url
+	mount_uploader :image_url, ImageUploader
+	#validates_format_of :button_url, :with => URI::regexp(%w(http https))
+	validates :button_url, presence: true, :format => {:with => URI.regexp}
+	validates :button_text,:format => { :with => /\A[a-zA-Z\-]+\z/},length: { maximum: 15 }
+	validates :image_text,:format => { :with => /\A[a-zA-Z\-]+\z/},length: { maximum: 50 }
+	validate :validate_minimum_image_size1
+    def validate_minimum_image_size1
+     image1 = MiniMagick::Image.open(image_url.path)
+      unless image1[:width] > 1400 && image1[:height] > 730
+        errors.add :image, "should be 1400x730px maximum!" 
+      end
+    end
+end
