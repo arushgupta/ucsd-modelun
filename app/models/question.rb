5a1b296595684635594947896744eb1c942de921
Arush Gupta <amg038@ucsd.edu>
2015-07-28 15:22:53 +0530

    Undid the comment on length validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,6 @@ class Question < ActiveRecord::Base
 	belongs_to :faq
 	
 	validates :question,  presence: true, length: { maximum: 200 }
-    validates :answer,  presence: true#, length: { maximum: 500 }
+    validates :answer,  presence: true, length: { maximum: 500 }
 
 end
