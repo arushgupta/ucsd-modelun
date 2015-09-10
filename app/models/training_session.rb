27c09983905eadb126c8fb4e433e81d927658910
anshu <anshu.sharma@netsolutions.in>
2015-09-10 12:17:53 +0530

    trainee_session and faq update

--- app/models/training_session.rb
+++ app/models/training_session.rb
@@ -15,35 +15,23 @@ class TrainingSession < ActiveRecord::Base
   validates :location,:presence => true, :format => { :with => /\A[a-zA-Z0-9\s\.',:(){}_\-]+\z/},length: { maximum: 100}
   validates :topic_image_url,:presence =>true
   validates :committee_image_url,:presence =>true
-  validate :date_validation1
-  validate :date_validation
   validates :start_time, presence: true
   validates :end_time, presence: true
-  
-  def date_validation
-    if end_date < start_date
-      errors[:end_date] << "cannot be before the start date"
-      return false
-    else
-      return true
-    end
-  end 
-
-  def date_validation1
-    if start_date < Date.today
-      errors[:start_date] << "cannot be before the today date"
-      return false
-    else
-      return true
-    end
-  end 
-
-  def date_validation
+  validate :time_validation
+   def date_validation
     if date < Date.today
       errors[:date] << "cannot be before the today date"
       return false
     else
       return true
     end
-  end  
+  end
+  def time_validation
+    if end_time < start_time
+      errors[:end_time] << "cannot be before the start time"
+      return false
+    else
+      return true
+    end
+  end 
 end
