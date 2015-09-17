1984c5d1e20efe4c361e048a3bedadd0de39bb27
Arush Gupta <arushgupta.me@gmail.com>
2015-09-17 11:56:31 +0530

    Files updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
       :address              => "smtp.sendgrid.net",
       :port                 => 587,
       :domain               => "heroku.com",
-      :user_name            => "usename",
-      :password             => "password",
+      :user_name            => "munadmin",
+      :password             => "munadmin123!",
 
       :authentication       => :plain,
       :enable_starttls_auto => true
