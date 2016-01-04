cee5632e7441f26ac5f941f3fcc666c361352a67
Pardeep Saini <pardeep.saini@netsolutionsindis.com>
2016-01-04 14:56:32 +0530

    fix ckeditor issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require ckeditor/config
+//= require ckeditor/init
 //= require ckeditor/filebrowser/javascripts/fileuploader.js
 //= require categories
 
