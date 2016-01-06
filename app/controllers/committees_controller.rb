28d29860c9bf4dce41af4547fd8c7fd85e1c0f96
Arush Gupta <arushgupta.me@gmail.com>
2016-01-06 04:16:40 -0800

    Attempt 3: Fixing file download path in committtee controller

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -2,7 +2,7 @@ class CommitteesController < ApplicationController
   
   def download
       send_file(
-        "#{Rails.root}/public/uploads/committee/#{params[:file]}",
+        "#{Rails.root}/public/uploads/committee/files/#{params[:file]}",
         filename: "#{params[:file]}",
         type: "application/pdf"
       )
